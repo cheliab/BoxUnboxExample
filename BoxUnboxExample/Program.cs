132e9a020db8696e606ae50634cdf26080685d5a
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2020-06-24 23:27:59 +1000

    Добавил простой пример упаков распаковки

--- BoxUnboxExample/Program.cs
+++ BoxUnboxExample/Program.cs
@@ -4,9 +4,23 @@ namespace BoxUnboxExample
 {
     class Program
     {
+        /// <summary>
+        /// Простой пример преобразования 
+        /// </summary>
+        public static void SimpleBoxingUnboxing()
+        {
+            int valueType = 5; // значение в стеке
+            object refType = valueType; // присваивание значения сопровождается упаковкой / значение на куче
+            int valueTypeUnboxed = (int) refType; // приведение к int вызовет распаковку / значение в стеке (в куче значение тоже осталось)
+            
+            Console.WriteLine(valueType);
+            Console.WriteLine(refType);
+            Console.WriteLine(valueTypeUnboxed);
+        }
+
         static void Main(string[] args)
         {
-            
+            SimpleBoxingUnboxing();
             
             Console.ReadKey();
         }
